123c58b7b512d8b6a614c430f53642b6edf3b479
Leo Budrinca <leo@readfwd.com>
2017-03-10 14:03:10 +0200

    Build my personal web page

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,37 @@
 $(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/sergiocruz.json',
+    dataType: 'jsonp',
+    success: function(data) {
+      populateWithCourses(data.courses.completed)
+    }
+  });
+  function populateWithCourses(courses) {
 
-  // your code will go here
+    var $badges = $('#badges');
 
+    courses.forEach(function(course) {
+
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
 });
